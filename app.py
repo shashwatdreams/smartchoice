1eec7cba8579fbb20433bd2fdb9fbf3a311c832f
Shashwat Sharma <kush.shashwat@outlook.com>
2025-03-01 15:40:03 -0600

    initializing

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title='Xfinity Personalized Recommendation System', layout='centered')
+
+st.title('Xfinity Personalized Recommendation System')
+
+st.subheader('Which Xfinity services are you interested in?')
+services = st.multiselect('Select one or more options:', ['Internet', 'Mobile', 'TV', 'Home Phone', 'Home Security'])
+
+if services:
+    st.success('Services selected: ' + ', '.join(services))
